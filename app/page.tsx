54433d06701b78f84e52e03cba49d1a949f5daf4
Shivam Panwar <panwarshivam028@gmail.com>
2025-12-31 16:59:34 +0530

    Updated code shadcn installed

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+const Homepage = () => {
+  return ( 
+    <>Pro store
+    <Button>Click Me</Button>
+    
+    </>
+   );
+}
+ 
+export default Homepage;
